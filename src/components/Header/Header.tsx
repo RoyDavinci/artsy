299f048115c6da02ee0e6a890f051f7d91cfdcbf
DavinciCode <emsthias33@gmail.com>
2022-12-08 16:41:33 +0100

    made changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,53 +1,136 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./header.css";
 import { Link } from "react-router-dom";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import CloseIcon from "@mui/icons-material/Close";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
 
 export const Header = () => {
 	const links = ["Home", "Marketplace", "Auctions", "Drop"];
 	const [indexValue, setIndexValue] = useState<number>(0);
+	const [isMobile, setIsMobile] = useState<boolean>(false);
+	const [show, setShow] = useState<boolean>(false);
 
 	const handleIndex = (value: number) => {
 		setIndexValue(value);
 	};
 
+	//choose the screen size
+	const handleResize = () => {
+		if (window.innerWidth < 860) {
+			setIsMobile(true);
+		} else {
+			setIsMobile(false);
+		}
+	};
+
+	// create an event listener
+	useEffect(() => {
+		window.addEventListener("resize", handleResize);
+		return () => {
+			console.log("cleared on unmount");
+		};
+	}, []);
+	console.log(isMobile);
+
 	return (
 		<header className='header__container lg:mx-32'>
-			<nav className='flex justify-between items-center'>
-				<h1>ARTSY</h1>
-				<ul className='flex'>
-					{links.map((item, index) => {
-						return (
-							<li key={index}>
-								<Link
-									to={
-										item.toLowerCase() === "home"
-											? "/"
-											: `/${item.toLowerCase()}`
-									}
-									className={`${indexValue === index ? "padding" : ""}`}
-									onClick={() => handleIndex(index)}
-								>
-									{item}
-								</Link>
-							</li>
-						);
-					})}
-				</ul>
-				<ul className='flex'>
-					<li>
-						<SearchOutlinedIcon />
-					</li>
-					<li>
-						<ShoppingCartOutlinedIcon />
-					</li>
-					<li>
-						<NotificationsOutlinedIcon />
-					</li>
-				</ul>
-			</nav>
+			{!isMobile ? (
+				<nav className='flex justify-between items-center'>
+					<h1>ARTSY</h1>
+					<ul className='flex'>
+						{links.map((item, index) => {
+							return (
+								<li key={index}>
+									<Link
+										to={
+											item.toLowerCase() === "home"
+												? "/"
+												: `/${item.toLowerCase()}`
+										}
+										className={`${indexValue === index ? "padding" : ""}`}
+										onClick={() => handleIndex(index)}
+									>
+										{item}
+									</Link>
+								</li>
+							);
+						})}
+					</ul>
+					<ul className='flex'>
+						<li>
+							<SearchOutlinedIcon />
+						</li>
+						<li>
+							<ShoppingCartOutlinedIcon />
+						</li>
+						<li>
+							<NotificationsOutlinedIcon />
+						</li>
+					</ul>
+				</nav>
+			) : (
+				<>
+					<div className='flex justify-between items-center mobile-nav'>
+						<div onClick={() => setShow(!show)}>
+							<MenuIcon />
+						</div>
+						<h1>ARTSY.</h1>
+						<div>
+							<ul className='flex'>
+								<li>
+									<SearchOutlinedIcon />
+								</li>
+								<li>
+									<ShoppingCartOutlinedIcon />
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div
+						className={
+							!show
+								? "sidebar__header__container p-6 absolute sidebar__header__container__close"
+								: "sidebar__header__container p-6 absolute"
+						}
+					>
+						<div className='flex justify-between items-center'>
+							<h1>ARTSY</h1>
+							<div
+								className='close__header__icon'
+								onClick={() => setShow(!show)}
+							>
+								<CloseIcon />
+							</div>
+						</div>
+						<ul className=''>
+							{links.map((item, index) => {
+								return (
+									<li key={index}>
+										<Link
+											to={
+												item.toLowerCase() === "home"
+													? "/"
+													: `/${item.toLowerCase()}`
+											}
+											className='text-2xl'
+											onClick={() => handleIndex(index)}
+										>
+											{item}
+										</Link>
+									</li>
+								);
+							})}
+						</ul>
+						<div className='header__chat__container'>
+							<ChatBubbleIcon />
+						</div>
+					</div>
+				</>
+			)}
 		</header>
 	);
 };
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./hero.css";
 
 export const Hero = () => {
+	const [isMobile, setIsMobile] = useState<boolean>(false);
+	//choose the screen size
+	const handleResize = () => {
+		if (window.innerWidth < 860) {
+			setIsMobile(true);
+		} else {
+			setIsMobile(false);
+		}
+	};
+
+	// create an event listener
+	useEffect(() => {
+		window.addEventListener("resize", handleResize);
+		return () => {
+			console.log("cleared on unmount");
+		};
+	}, []);
+	console.log(isMobile);
 	return (
-		<section className='hero__container__items flex flex-col justify-center items-center my-10'>
-			<h1>Photography is poetry & beautiful untold stories</h1>
-			<p>
+		<section className='hero__container__items flex flex-col justify-center items-center text-center lg:my-10'>
+			<h1 className={isMobile ? "mobileScreenHeader" : "largeScreenHeader"}>
+				Photography is poetry & beautiful untold stories
+			</h1>
+			<p
+				className={isMobile ? "mobileScreenParagraph" : "largeScreenParagraph"}
+			>
 				Flip through more than 10,000 vintage shots, old photograghs, historic{" "}
-				<br />
+				{isMobile ? "" : <br />}
 				images and captures seamlessly in one place. Register to get top access.
 			</p>
 		</section>
